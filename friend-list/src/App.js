4586ec9f4ce69ea96568dc1fa32db199dcbdb48b
Drew Yuri <drewyuri@Drews-Air.attlocal.net>
2019-12-16 13:50:55 -0800

    3 unique people with diff names and jobs

--- friend-list/src/App.js
+++ friend-list/src/App.js
@@ -4,19 +4,40 @@ import './App.css';
 const App = () => <PersonList />;
 
 const PersonList = () => {
-  return <Person />;
-};
-const Person = () => {
-  const url =
-    "https://randomuser.me/api/portraits/thumb/men/22.jpg"
 
+const people = [
+  {
+    img:22,name:'john',job:'Developer'
+  },
+  {
+    img:34,name:'Bob',job:'Designer'
+  },
+  {
+    img:56,name:'Peter',job:'Artist'
+  }
+];
+
+  return (<section>
+    <Person person={people[0]} />;
+    <Person person={people[1]}>
+      Lorem ipsum dolor sit amet consector adispiact ekit.
+      </Person>
+    <Person person={people[2]} />;
+  </section>)
+};
+const Person = (props) => {
+  const {img,name,job} = props.person;
+  const {children } = props;
+  const url =
+    `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
 
   return <div className="person">
     <img src={url} alt=""/>
-  <div>
-    <h4>name</h4>
-    <h4>job</h4>
-  </div>
+      <div>
+        <h4>{name}</h4>
+        <h4>{job}</h4>
+        {children}
+      </div>
     </div>;
 };
 
